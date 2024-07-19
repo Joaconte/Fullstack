3aa7e06cb769cd43fb1008415c14aef79cfdff82
Joaquin Conte Grand <43884898+Joaconte@users.noreply.github.com>
2024-07-19 13:25:01 -0300

    3.10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-var morgan = require('morgan')
+const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 
 morgan.token('body', (req, res) => { 
@@ -116,7 +120,7 @@ let persons = [
     response.json(person)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
