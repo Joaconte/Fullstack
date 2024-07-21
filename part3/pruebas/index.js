cb89559b4f68b6e5c9c361a89b3184e90f99d80b
Joaquin Conte Grand <43884898+Joaconte@users.noreply.github.com>
2024-07-21 12:34:32 -0300

    3.12

--- part3/pruebas/index.js
+++ part3/pruebas/index.js
@@ -6,32 +6,31 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+const mongoose = require('mongoose')
+
+const url =
+  `mongodb+srv://jconte:${process.env.MONGO_PASSWORD}@cluster0.dyjmjnk.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
   
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
-
   app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
